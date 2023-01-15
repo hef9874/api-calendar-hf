386e993c990581cecdb3f4335d3ad90a9551a219
nah <hef9874@gmail.com>
2023-01-15 14:12:56 -0500

    added months to js and current date and time

--- Develop/script.js
+++ Develop/script.js
@@ -35,11 +35,52 @@ $(function() {
     }
   }
 
+  //what happens when the add event button is clicked - data stored in JSON
   hourContainer.children("button").on('click', event => {
     var addButton = $(event.target).parent();
-
-    $('.content')
-  })
+    $('.content').text('Added to Calendar!');
+    var containerId = $(addButton).attr('id');
+    var getText = $(addButton).children('textarea');
+    var newContent = getText.val();
+    localStorage.setItem(containerId, JSON.stringify(newContent));
+  });
 
 });
+//getting time and day & AM and PM
+function getTime () {
+  var currentDate = new Date();
+  var month = getMonth(currentDate.getMonth());
+  var dayTime = "AM";
+  var currentDay = month + '' + currentDate.getDate() + ',' + currentDate.getFullYear();
+
+  var hourNow = currentDate.getHours();
+  if(hourNow > 12) {
+    dayTime = 'PM';
+    // hourNow -=12;
+  }
+
+  var minuteNow = String(currentDate.getMinutes()).padStart(2.0);
+  var timeNow = hourNow + ':' + minuteNow + '' + dayTime;
+
+  return currentDay + " - " + timeNow;
+}
+
+getMonth = (thisMonth) => {
+  switch(thisMonth) {
+  case 0: return 'January';
+  case 1: return 'February';
+  case 2: return 'March';
+  case 3: return 'April';
+  case 4: return 'May';
+  case 5: return 'June';  
+  case 6: return 'July';
+  case 7: return 'August';
+  case 8: return 'September';
+  case 9: return 'October';
+  case 10: return 'November';
+  case 11: return 'December';
+  }
+}
+
+
 
